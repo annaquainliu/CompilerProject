456d1d832d2b614893e47dcda2a1e1b7ca57c18e
Xavier deSouza <103150835+xdesou01@users.noreply.github.com>
2024-03-01 17:17:27 -0500

    Tentatively finished
    Still need to test and tidy up

--- type-inference.ml
+++ type-inference.ml
@@ -100,15 +100,54 @@ let ftvs tau =
     | CONAPP(ty, tys) -> List.fold_left getftvs (getftvs curSet ty) tys
   in getftvs [] tau
 
-(* let findTyscheme s gamma = 
-  let rec locate g = match g with
-  | []               -> raise (Cringe "var inference led to missing type scheme")
-  | (s', scheme)::xs -> if s' = s then scheme else locate xs
-  in locate gamma *)
+let ftvsGamma(_, free) = free
+
+let findTyscheme s gamma = match gamma with
+  | (env, free) ->
+    let rec locate g = match g with
+    | []               -> raise (Cringe "var inference led to missing type scheme")
+    | (s', scheme)::xs -> if s' = s then scheme else locate xs
+    in locate env
 
 let bindtyscheme (x, ts, (g, f)) = match ts with 
   | FORALL(bound, t) -> ((x, ts)::g, union f (diff (ftvs t) bound))
 
+let rec bindList ys zs l = match (ys, zs) with
+  | (w::ws, x::xs) -> bindList ws xs ((w, x)::l)
+  | ([], [])       -> l
+  | _              -> raise typeInferenceBug
+
+let instantiate ts tys = match ts with
+  | FORALL(bound, t) -> tysubst (bindList bound tys []) t
+
+let freshInstance ts = match ts with
+  | FORALL(bound, tau) -> instantiate ts (List.map freshtyvar bound)
+
+let rec conjoin c = match c with 
+  | []       -> TRIVIAL
+  | [c1]     -> c1
+  | curC::cs -> curC ^ (conjoin cs)
+
+let canonify ts = match ts with
+  | FORALL(bound, tau) ->
+    let genTyvarName n = String.cat "t" (string_of_int n) in
+    let freenut = diff (ftvs tau) bound in
+    let rec nextIdx n =
+      if member (genTyvarName n) freenut then nextIdx (n + 1) else n in
+    let rec newVars idx olds = match olds with
+      | []    -> []
+      | o::os -> let n = nextIdx idx in
+        genTyvarName n :: (newVars(idx + 1) os) in
+    let newBoundVars = newVars 0 bound in
+    FORALL(newBoundVars, tysubst (bindList bound (List.map (fun x -> TYVAR x) newBoundVars) []) tau)
+    
+
+let generalize vars tau = 
+  canonify (FORALL (diff (ftvs tau) vars, tau))
+
+let confirmLambda e = match e with
+  | LAMBDA(_, _) -> e
+  | _            -> raise typeInferenceBug
 
 let rec solve c = match c with
   | TRIVIAL      -> []
@@ -124,33 +163,36 @@ let rec solve c = match c with
     | (TYCON _, CONAPP _)  -> raise typeInferenceBug
     | (CONAPP(t1, t1s), CONAPP(t2, t2s)) -> 
         let rec zip l1 l2 acc = match (l1, l2) with
-        | ([], [])       -> List.reverse acc
-        | (x::xs, y::ys) -> zip(xs ys ((x, y)::acc))
+        | ([], [])       -> List.rev acc
+        | (x::xs, y::ys) -> zip xs ys ((x, y)::acc)
         | _              -> raise typeInferenceBug in
-        let zipped = zip t1s t2s [] in
-        List.fold_left(fun (t, t') acc -> compose(acc, solve(consubst acc (t ^^ t')))) (t1 ^^ t2) zipped
+        let zipped = zip t1s t2s [] in 
+        List.fold_left(fun acc (t, t') -> compose(acc, solve(consubst acc (t ^^ t')))) (solve(t1 ^^ t2)) zipped
     | _                    -> solve (t2 ^^ t1)
-
-
+  
 let rec typeof exp g = 
   let rec infer ex = match ex with
   | LITERAL(value) -> inferLiteral value
-  | VAR(s) -> (strty, TRIVIAL)
+  | VAR(s) -> (freshInstance (findTyscheme s g), TRIVIAL)
   | IF(e1, e2, e3) ->
       let (t1, c1) = infer e1 in
       let (t2, c2) = infer e2 in
       let (t3, c3) = infer e3 in
-        (t2, c1 ^ c2 ^ c3)
+        (t2, c1 ^ c2 ^ c3 ^ (t2 ^^ t3) ^ (t1 ^^ boolty))
   | LAMBDA(formals, b) -> 
       let alphas = List.map (fun form -> (form, freshtyvar())) formals in
       let newG = List.fold_left (fun acc (x, c) -> bindtyscheme (x, FORALL ([], c), acc)) g alphas in
       let (endty, endc) = typeof b newG in (funtype ((List.map snd alphas), endty), endc)
+
+  | LET(ds, e) -> 
+    let (defsC, finalGamma) = List.fold_left (fun (curC, curG) d -> 
+        let (_, nextC, nextG, _) = typeOfDef d curG in
+        (nextC, nextG)) (TRIVIAL, g) ds in
+        let (tau, expC) = typeof e finalGamma in (tau, expC ^ defsC)      
   | APPLY(f, es) -> match (typesof (f::es) g) with
       | ([], _) -> raise (Cringe "invalid")
       | (fty::etys, con) -> let crisp = freshtyvar() in
         (crisp, con ^ fty ^^ (funtype (etys, crisp)))
-
-  | _ -> (boolty, TRIVIAL)
 
   and inferLiteral v = match v with
   | STRING(_) -> (strty, TRIVIAL)
@@ -166,6 +208,84 @@ let rec typeof exp g =
          let (curty, curc) = typeof e g in
          (curty::ts, c ^ curc)) ([], TRIVIAL) es
 
-
-
   in infer exp
+
+
+(* (tyscheme, con, type env, output string) *)
+and typeOfDef d g = 
+  let rec inferDef def = match def with 
+    | LETDEF(n, e) -> 
+      let (tau, c) = typeof e g in
+      let theta = solve c in
+      let crisps = inter (domain theta) (ftvsGamma g) in
+      let finalC = c ^ conjoin(List.map (fun x -> TYVAR x ^^ varsubst theta x) crisps) in
+      let ligma = generalize (ftvsGamma g) ((tysubst theta) tau) in
+      let newGamma = bindtyscheme(n, ligma, g) in
+      (ligma, finalC, newGamma, "")
+      
+    (* possible bug due to using newGamma underneath its definition? type rules say to use gamma *)
+    | LETREC(n, e) -> 
+      let _ = confirmLambda e in
+      let alpha = freshtyvar() in
+      let newGamma = bindtyscheme(n, FORALL([], alpha), g) in
+      let (tau, c) = typeof e newGamma in
+      let c2 = c ^ (alpha ^^ tau) in
+      let theta = solve c2 in
+      let crisps = inter (domain theta) (ftvsGamma newGamma) in
+      let finalC = c2 ^ conjoin(List.map (fun x -> TYVAR x ^^ varsubst theta x) crisps) in
+      let ligma = generalize (ftvsGamma newGamma) ((tysubst theta) tau) in
+      let finalGamma = bindtyscheme(n, ligma, g) in
+      (ligma, finalC, finalGamma, "")
+
+    | EXP(e) -> typeOfDef (LETDEF ("it", e)) g
+  in inferDef d
+
+let rec printType t = match t with 
+  | TYVAR(a) -> print_string a
+  | TYCON(c) -> print_string c
+  | CONAPP(TYCON tc, [ty]) -> (*lists currently the only CONAPP application*)
+    let () = print_string tc in let () = print_string " " in printType ty
+  | _ -> print_string "Margaret Thatcher"  
+
+let printExpType e =
+  let (tau, _) = typeof e ([], []) in let () = printType tau in print_endline ""
+
+let rec printConstraint c = match c with
+  | TRIVIAL -> print_string "TRIVIAL"
+  | EQ(t1, t2) -> 
+      let () = print_string "(" in
+      let () = printType t1 in
+      let () = print_string " ~ " in
+      let () = printType t2 in print_string ")"
+  | CONJ(c1, c2) ->
+      let () = print_string "(" in
+      let () = printConstraint c1 in
+      let () = print_string " ^ " in
+      let () = printConstraint c2 in print_string ")"
+
+(*returns string forms of type and constraint*)
+let debugExpType e = 
+  let (tau, c) = typeof e ([], []) in
+  let () = printType tau in let () = print_string ", " in let () = printConstraint c in print_endline ""
+
+
+
+(*type inference unit tests*)
+
+let () = let () = print_string "type1 is... " in printExpType (LITERAL (NUMBER 666))
+let () = let () = print_string "type2 is... " 
+         in printExpType (IF (LITERAL(BOOLV true), LITERAL (NUMBER 420), LITERAL (NUMBER 666)))
+let () = let () = print_string "type3 is... " 
+         in printExpType (IF (LITERAL(BOOLV true), LITERAL (STRING "xd"), LITERAL (STRING "XD")))
+let () = let () = print_string "type4 (should fail) is... " 
+        in printExpType (IF (LITERAL(NUMBER 0), LITERAL (STRING "xd"), LITERAL (STRING "XD")))
+
+
+(*type inference debugging*)
+
+let () = let () = print_string "debug test1: " 
+        in debugExpType (IF (LITERAL(NUMBER 0), LITERAL (STRING "xd"), LITERAL (STRING "XD")))
+let wtf = 
+  let (ty, c) = typeof (IF (LITERAL(NUMBER 0), LITERAL (STRING "xd"), LITERAL (STRING "XD"))) ([], []) in
+  solve c
+let wtf2 = let (ty, c) = typeof(NIL) in solve c
